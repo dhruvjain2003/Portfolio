51b4d24963f5c48e15fe24485cd43e135a40c451
Dhruv Jain <dhruv.jain@tothenew.com>
2025-05-09 22:10:50 +0530

    fixed some font and added url

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -100,8 +100,8 @@ const ProjectsSection = () => {
   };
 
   return (
-    <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-1 sm:mb-8 md:mb-12">
+    <section id="projects" className="scroll-mt-20">
+      <h2 className="text-center text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white mt-2 sm:mt-4 mb-2 sm:mb-8 md:mb-12">
         My Projects
       </h2>
       <div className="text-white flex flex-wrap justify-center items-center gap-2 py-6 px-4">
