f1cc97073396bc6773313da8ef47cfda296cd07d
Dhruv Jain <dhruv.jain@tothenew.com>
2025-05-10 12:48:54 +0530

    fixed css

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-3xl md:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl md:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
@@ -26,7 +26,7 @@ const HeroSection = () => {
                 1000,
                 "Web Developer",
                 1000,
-                "UI/UX Designer",
+                "Tech Builder",
                 1000,
               ]}
               wrapper="span"
--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -12,7 +12,9 @@ const ProjectTag = ({ name, onClick, isSelected }) => {
         px-4 py-2 text-sm 
         sm:px-5 sm:py-2.5 sm:text-base 
         md:px-6 md:py-3 md:text-xl 
-        cursor-pointer transition-all duration-200`}
+        cursor-pointer transition-all duration-200
+          focus:outline-none focus:ring-2 focus:ring-primary-500
+  hover:text-white hover:bg-primary-500/10`}
       onClick={() => onClick(name)}
     >
       {name}
